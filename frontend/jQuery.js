37d026e79736e65596bf676a80d459872d75dafa
Iñigo Moreno <ignigomoreno@gmail.com>
2016-10-08 11:01:47 -0600

    Implemented fronted user login and roomquestion response.

--- frontend/jQuery.js
+++ frontend/jQuery.js
@@ -6,21 +6,59 @@ function getUrlVars() {
 	return vars;
 }
 
-var id = getUrlVars()["id"];
+var roomID = getUrlVars()["id"];
 var server = "http://localhost:5000";
 $(document).ready(function(){
 	$("#emailInput").focus();
 	$(document).keypress(function(e){
 		if (e.which == 13){
 			var email = $("#emailInput").val();
-			$.get(server + "/joinroom&idroom="+id+"&email="+email,function(data){
-				alert("Data: "+data);
+			$.get(server + "/joinRoom?idRoom="+roomID+"&email="+email,function(data){
+				serverRepply=JSON.parse(data);
+				var userID = serverRepply.id;
+				startQuestions(userID,roomID);
+				$(document).off("keypress");
 			});
 		}
 	});
-	/*$("button").click(function(){
-		$.get(server+"/register",function(data,status){
-			alert("Data: " + data + "\nStatus: " + status);
-		});
-	});*/
 });
+
+function startQuestions(userID, roomID){
+	$("#emailContainer").fadeOut();
+
+	var questions;
+
+	$("#questionContainer").fadeIn();
+	$.get(server+"/getRoomQuestion?idRoom="+roomID+"&idUser="+userID, function(data){
+		serverRepply=JSON.parse(data);
+		questions=serverRepply.questions;
+		var answers=[];
+		setQuestions(userID,roomID,questions,answers,0);
+	});
+}
+
+function setQuestions(userID,roomID,questions,answers,i){
+	$("#answerInput").val("").focus();
+	if (i==questions.length){
+		$.post(server+"/postRoomAnswers",{
+			idRoom:roomID,
+			idUser:userID,
+			answers:answers
+		});
+		alert(answers[0].text);
+		alert(answers[1].text);
+	}
+	else{
+		$("#questionText").text(questions[i].text);
+		$(document).keypress(function(e){
+			if (e.which==13){
+				answers[i]={
+					id:questions[i].id,
+					text:$("#answerInput").val()
+				};
+				$(document).off("keypress");
+				setQuestions(userID,roomID,questions,answers,i+1);
+			}
+		});
+	}
+}
