1b6097921986c81dc37cf8ede6c7494b8483dfb6
jmigual <j.marce.igual@gmail.com>
2016-10-08 03:36:01 +0200

    Added more stubs

--- backend/socialQuiz.py
+++ backend/socialQuiz.py
@@ -6,14 +6,56 @@ import json
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-	return "Hello world"
+    return "Hello world"
+
 
 @app.route('/register/')
 def register():
-	return json.dumps({ "id" : 12345678 })
+    # To obtain the mail
+    email = request.args.get('email')
+    return json.dumps({"id": 12345678})
+
+
+@app.route('/joinRoom')
+def join_room():
+    idRoom = request.args.get('idRoom')
+    email = request.args.get('email')
+    return json.dumps({"id": 12345678})
+
+
+@app.route('/getUserId')
+def get_user_id():
+    email = request.args.get('email')
+    return json.dumps({"id": 12345678})
+
+
+@app.route('/createRoom')
+def create_room():
+    id = request.args.get('id')
+    return json.dumps({"id": 123 })
+
+
+@app.route('/fillRoom')
+def fill_room():
+    print(request.form())
+
+
+@app.route('/getRoomQuestion')
+def get_room_question():
+    idRoom = request.args.get('idRoom')
+    return json.dumps({"questions": [
+        {"id": 2113, "text": "How do you feel?"},
+        {"id": 2114, "text": "Do you want a kiss?"}]})
+
+
+@app.route('/postRoomAnswers')
+def post_room_answers():
+    print(request.form())
+
 
 
 if __name__ == '__main__':
-	app.run(debug=True, host='127.0.0.1')
+    app.run(debug=True, host='127.0.0.1')
