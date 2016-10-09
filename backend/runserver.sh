30ca25a45fd2c53218ada8ee900a4b5666df53ce
jmigual <j.marce.igual@gmail.com>
2016-10-09 03:27:07 +0200

    Fixed server script

--- backend/runserver.sh
+++ backend/runserver.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-. venv/bin/activavte
-python3 -m flask run -host=0.0.0.0 > log.txt 2> err.txt &
+. venv/bin/activate
+
+export FLASK_APP=socialQuiz.py
+
+python3 -m flask run --host=0.0.0.0 > log.txt 2> err.txt &
